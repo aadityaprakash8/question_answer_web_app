367eb945aa8de83a695f9682141c287450bd9d73
aadityaprakash8 <prakashaaditya8@gmail.com>
2021-05-27 07:26:40 +0530

    Implemented routing, login, add questions.

---
+++ schema.sql
@@ -0,0 +1,15 @@
+create TABLE users (
+    id INTEGER PRIMARY KEY autoincrement,
+    name text not null,
+    password text not null,
+    expert boolean not null,
+    admin boolean not null
+);
+
+create table questions(
+    id integer primary key autoincrement,
+    question_text text not null,
+    answer_text text,
+    asked_by_id integer not null,
+    experts_id integer not null
+);
